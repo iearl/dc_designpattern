2032617fdfd34e83c3f47c2957da44ffc567d044
iearl <dc_magaowei@163.com>
2018-11-26 17:34:15 +0800

    建造者模式

--- src/main/java/com/ants/creational/builder/IBuilderComputer.java
+++ src/main/java/com/ants/creational/builder/IBuilderComputer.java
@@ -1,5 +1,8 @@
 package com.ants.creational.builder;
 
+/**
+ * 构建者，可以与工厂模式配合胜场对象
+ */
 public interface IBuilderComputer {
     Keyboard builderKeyboard();
 
--- src/main/java/com/ants/creational/builder/IDirectorComputer.java
+++ src/main/java/com/ants/creational/builder/IDirectorComputer.java
@@ -1,5 +1,8 @@
 package com.ants.creational.builder;
 
+/**
+ * 装配
+ */
 public interface IDirectorComputer {
 
     Computer directorComputer();
